b8c52c46e3b6c0dfce05ee8bff4cd8ce2b22beef
jordan <jharrison1@qa.com>
2021-09-10 11:50:00 +0100

    added solutions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import Swapi from './components/faff/Swapi';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import King from './components/King';
+import MyComponent from './components/props/MyComponent';
 import Person from './components/props/Person';
 
 const App = () => {
 
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/>
       <Person name="Jordan" age={27} jobTitle="Herder of cats"/>
       <Person name="Seb" age={24} jobTitle="Peddler of conspiracies"/>
       <Person/>
-      <Footer/>
+      <Footer/> */}
+      {/* <MyComponent/> */}
+      <King nm="Barry" ctry="Coventry" yrs="1066" hse="straw"/>
+      <King nm="Ryan" ctry="devon" yrs="1999-2021" hse="Donaghue"/>
     </div>
   );
 }
--- src/components/King.jsx
+++ src/components/King.jsx
@@ -1,19 +1,21 @@
-const King = () => {
-    const king = {
-        nm: 'Barry',
-        ctry: 'Coventry',
-        yrs: 1066,
-        hse: 'Brick'
-      }
+import PropTypes from 'prop-types';
 
+const King = ({nm, ctry, yrs, hse}) => {
       return (
         <>
-            <h3>{king.nm}</h3>
-            <p>Country: {king.ctry}</p>
-            <p>Years: {king.yrs}</p>
-            <p>House: {king.hse}</p>
+            <h3>{nm}</h3>
+            <p>Country: {ctry}</p>
+            <p>Years: {yrs}</p>
+            <p>House: {hse}</p>
         </>
       );
+}
+
+King.propTypes = {
+    nm: PropTypes.string.isRequired,
+    ctry: PropTypes.string.isRequired,
+    yrs: PropTypes.string.isRequired,
+    hse: PropTypes.string.isRequired,
 }
 
 export default King;
